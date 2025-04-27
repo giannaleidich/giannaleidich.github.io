957f2d069c21b6756f71607d9669bb5424c8815e
Gianna <167443345+giannaleidich@users.noreply.github.com>
2025-04-26 22:29:46 -0400

    Add files via upload
    batch 1

--- flare/Data/Search.js
+++ flare/Data/Search.js
@@ -1,1 +1,1 @@
-define({r:0.5,st:'Stem',n:1,t:[0],m:[],u:['../Content/A-Introduction-Topics/Introduction.htm'],s:['1'],p:[['1',5],['ornare',1]]});
+define({r:0.5,st:'Stem',n:1,t:[0],m:[],u:['../Content/accessibility-in-text/accessibility-in-text.htm'],s:['1'],p:[['1',3],['helped',12],['the',12]]});
