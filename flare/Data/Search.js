eff014d05a281ddcff42f3b4be35400c7a11f733
Gianna <167443345+giannaleidich@users.noreply.github.com>
2025-05-09 21:38:22 -0400

    Add files via upload

--- flare/Data/Search.js
+++ flare/Data/Search.js
@@ -1,1 +1,1 @@
-define({r:0.5,st:'Stem',n:1,t:[0],m:[],u:['../Content/accessibility-in-text/accessibility-in-text.htm'],s:['1'],p:[['1',3],['horizontal',6],['this',5]]});
+define({r:0.5,st:'Stem',n:1,t:[0],m:[],u:['../Content/accessibility-in-text/accessibility-in-text.htm'],s:['1'],p:[['1',3],['here',8],['therefore',4]]});
